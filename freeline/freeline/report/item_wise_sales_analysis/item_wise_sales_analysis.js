a52c5cb70993b7eabd693e3c662e07789872bc12
RAFI <infinityprogrammer01@gmail.com>
2022-08-21 11:09:16 +0400

    sale analysis

---
+++ freeline/freeline/report/item_wise_sales_analysis/item_wise_sales_analysis.js
@@ -0,0 +1,95 @@
+// Copyright (c) 2022, RAFI and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+// ------------------------------------------------
+//  App/Module : Item Wise Sales Analysis
+//  Developed By : MUHAMMED RAFI A
+//  Email : muhammedrafikkl@gmail.com
+//  Github : infinityprogrammer
+//  LICENSE : MIT 
+// ------------------------------------------------
+
+frappe.query_reports["Item Wise Sales Analysis"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1,
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+		},
+		{
+			"fieldname": "customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer"
+		},
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname": "mode_of_payment",
+			"label": __("Mode of Payment"),
+			"fieldtype": "Link",
+			"options": "Mode of Payment"
+		},
+		{
+			"fieldname": "warehouse",
+			"label": __("Warehouse"),
+			"fieldtype": "Link",
+			"options": "Warehouse"
+		},
+		{
+			"fieldname": "brand",
+			"label": __("Brand"),
+			"fieldtype": "MultiSelectList",
+			"options": "Brand",
+			get_data: function(txt) {
+				return frappe.db.get_link_options("Brand", txt);
+			},
+		},
+		{
+			"fieldname": "sales_person",
+			"label": __("Sales Person"),
+			"fieldtype": "MultiSelectList",
+			"options": "Sales Person",
+			get_data: function(txt) {
+				// return frappe.db.get_link_options("Sales Person", txt);
+				// frappe.query_report.set_filter_value('is_group', 0);
+				return frappe.db.get_link_options("Sales Person", txt, {is_group:0});
+			},
+			"reqd": 1,
+		},
+		{
+			"fieldname": "item_group",
+			"label": __("Item Group"),
+			"fieldtype": "Link",
+			"options": "Item Group"
+		},
+		{
+			"label": __("Group By"),
+			"fieldname": "group_by",
+			"fieldtype": "Select",
+			"options": ["Customer Group", "Customer", "Item Group", "Item", "Territory", "Invoice"]
+		}
+	],
+	"formatter": function(value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (data && data.bold) {
+			value = value.bold();
+
+		}
+		return value;
+	}
+}
