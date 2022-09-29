ffa4d6ec72c5629c3d7ca0481f2c28d453619434
RAFI <infinityprogrammer01@gmail.com>
2022-09-29 14:57:26 +0400

    api permission fix

--- freeline/freeline/globalapi.py
+++ freeline/freeline/globalapi.py
@@ -111,12 +111,31 @@ def get_item_barcode(item_code):
 @frappe.whitelist()
 def get_invoice_items_by_driver(driver=None):
     
-    condition =""
+    log_user = frappe.session.user
+    condition = ""
+    sales_person_head = frappe.db.get_value('User Permission', {'user': log_user,'allow':'Sales Person'}, ['for_value'],as_dict=1)
+    if sales_person_head:
+        sales_p = []
+        sales_p.append("A")
+        
+        lft, rgt = frappe.db.get_value("Sales Person", sales_person_head.for_value, ["lft", "rgt"])
+        sales_pers = frappe.db.sql(
+            """
+            select employee from `tabSales Person` where lft >= %s and rgt <= %s and is_group = 0
+            """,
+            (lft, rgt),
+            as_dict=1,
+        )
+        for s in sales_pers:
+            sales_p.append(s.employee)
+            
+        condition += " and employee in %(employee_id)s"
+        
     if driver:
         condition += " and driver = %(driver)s"
     
     sale_invoice_name = frappe.db.sql(""" SELECT name,posting_date,customer,driver FROM `tabSales Invoice`
-                                where docstatus = 1 {condition}""".format(condition=condition),{'driver':driver}, as_dict=True)
+                                where docstatus = 1 {condition}""".format(condition=condition),{'driver':driver,'employee_id':sales_p}, as_dict=True)
     sales_invoice_items = []
     for d in sale_invoice_name:
         item_details = frappe._dict()
