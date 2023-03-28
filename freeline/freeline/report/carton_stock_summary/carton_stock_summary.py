028fcc2c86888e5c04a4ec89cb2fc0557f478c69
RAFI <infinityprogrammer01@gmail.com>
2023-03-28 09:23:18 +0400

    age report

--- freeline/freeline/report/carton_stock_summary/carton_stock_summary.py
+++ freeline/freeline/report/carton_stock_summary/carton_stock_summary.py
@@ -21,13 +21,17 @@ def get_columns():
 		_("Company") + ":Link/Company:250",
 		_("Warehouse") + ":Link/Warehouse:150",
 		_("Item") + ":Link/Item:150",
+		_("Item Name") + ":Data:150",
+		_("Arabic Item Name") + ":Data:150",
 		_("Description") + "::300",
+		_("Brand") + ":Link/Brand:150",
 		_("Current Qty") + ":Float:100",
 		_("Barcode") + ":Data:100",
 		_("Supplier Ref") + ":Data:100",
-		_("Qty in Carton") + ":Float:100",
+		# _("Qty in Carton") + ":Float:100",
 		_("Highest UOM") + ":Link/UOM:130",
-		_("Highest UOM Factor") + ":Float:140",
+		_("Hi-UOM Factor") + ":Float:140",
+		_("QTY Hi-UOM") + ":Float:140",
 	]
 
 	return columns
@@ -54,14 +58,18 @@ def get_total_stock(filters):
 			SELECT
 				%s,
 				item.item_code,
+				item.item_name,
+				item.item_arabic_name,
 				item.description,
+				item.brand,
 				sum(ledger.actual_qty) as actual_qty,
 				(SELECT GROUP_CONCAT(barcode) FROM `tabItem Barcode` where `tabItem Barcode`.parent = item.name) as barcode,
 				(select GROUP_CONCAT(supplier_part_no) from `tabItem Supplier` where `tabItem Supplier`.parent = item.name) as supplier_no,
 				-- (SELECT conversion_factor FROM `tabUOM Conversion Detail` uf where uf.parent = item.item_code and uf.uom = 'Carton') as carton_factor,
-    			((sum(ledger.actual_qty))/ (SELECT conversion_factor FROM `tabUOM Conversion Detail` uf where uf.parent = item.item_code and uf.uom = 'Carton')) as qty_in_carton,
+    			-- ((sum(ledger.actual_qty))/ (SELECT conversion_factor FROM `tabUOM Conversion Detail` uf where uf.parent = item.item_code and uf.uom = 'Carton')) as qty_in_carton,
 				(SELECT uom FROM `tabUOM Conversion Detail` um where um.parent = item.item_code order by conversion_factor desc limit 1)highest_uom,
-				(SELECT conversion_factor FROM `tabUOM Conversion Detail` um where um.parent = item.item_code order by conversion_factor desc limit 1)highest_uom_factor
+				(SELECT conversion_factor FROM `tabUOM Conversion Detail` um where um.parent = item.item_code order by conversion_factor desc limit 1)highest_uom_factor,
+				((sum(ledger.actual_qty))/ (SELECT conversion_factor FROM `tabUOM Conversion Detail` um where um.parent = item.item_code order by conversion_factor desc limit 1)) as qty_in_huom
 			FROM
 				`tabBin` AS ledger
 			INNER JOIN `tabItem` AS item
