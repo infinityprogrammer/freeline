fe996b5421e46cfa3932d6d93920ab3864aed692
RAFI <infinityprogrammer01@gmail.com>
2023-04-25 14:37:20 +0400

    sales report mail

---
+++ freeline/freeline/report/item_wise_sales_for_mail/item_wise_sales_for_mail.js
@@ -0,0 +1,70 @@
+// Copyright (c) 2023, RAFI and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Item Wise Sales For Mail"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1,
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+		},
+		{
+			"fieldname": "customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer"
+		},
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname": "brand",
+			"label": __("Brand"),
+			"fieldtype": "MultiSelectList",
+			"options": "Brand",
+			get_data: function(txt) {
+				return frappe.db.get_link_options("Brand", txt);
+			},
+		},
+		{
+			"fieldname": "sales_person",
+			"label": __("Sales Person"),
+			"fieldtype": "MultiSelectList",
+			"options": "Sales Person",
+			get_data: function(txt) {
+				// return frappe.db.get_link_options("Sales Person", txt);
+				// frappe.query_report.set_filter_value('is_group', 0);
+				return frappe.db.get_link_options("Sales Person", txt);
+			},
+			"reqd": 1,
+		},
+		{
+			"fieldname": "item_group",
+			"label": __("Item Group"),
+			"fieldtype": "Link",
+			"options": "Item Group"
+		}
+	],
+	"formatter": function(value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (data && data.bold) {
+			value = value.bold();
+
+		}
+		return value;
+	}
+};
